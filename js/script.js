d760c71aad62538b69f163fd8986f89f2bb9b85e
k-sugawa <sugawakumiko@gmail.com>
2023-09-21 23:24:28 +0900

    Check required field修正

--- js/script.js
+++ js/script.js
@@ -22,24 +22,23 @@ function showSuccess(input) {
 function checkEmail(input) {
     const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     if (re.test(input.value.trim())) {
-        showSuccess(input);
+      showSuccess(input);
     } else {
-        showError(input, 'Email is not valid');
+      showError(input, 'Email is not valid');
     }
-}
+  }
 
 //Check required field
 function checkRequired(inputArr) {
     let isRequired = false;
-    inputArr.forEach(function (input) {
+    inputArr.forEach(function(input) {
         if (input.value.trim() === '') {
-            showError(input, `${getFieldName(input)}) is required `);
+            showError(input, `${getFieldName(input)} is required`);
             isRequired = true;
         } else {
             showSuccess(input);
         }
     });
-    
 
     return isRequired;
 }
