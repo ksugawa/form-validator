b2059962d69918b755c23320990709eade161014
k-sugawa <sugawakumiko@gmail.com>
2023-09-21 23:19:17 +0900

    Show input error message箇所の修正

--- js/script.js
+++ js/script.js
@@ -4,10 +4,10 @@ const email = document.getElementById('email');
 const password = document.getElementById('password');
 const password2 = document.getElementById('password2');
 
-//Show input error message
+// Show input error message
 function showError(input, message) {
     const formControl = input.parentElement;
-    formControl.className = 'form-control error'
+    formControl.className = 'form-control error';
     const small = formControl.querySelector('small');
     small.innerText = message;
 }
