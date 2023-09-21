62d605ff86df56a4ce5054c7a2d21f826b669ef4
k-sugawa <sugawakumiko@gmail.com>
2023-09-21 23:29:19 +0900

    Event listeners修正

--- js/script.js
+++ js/script.js
@@ -75,11 +75,12 @@ function getFieldName(input) {
 //Event listeners
 form.addEventListener('submit', function(e) {
     e.preventDefault();
-
+  
     if(checkRequired([username, email, password, password2])){
-        checkLength(username, 3, 15);
-        checkLength(password, 6, 25);
-        checkEmail(email);
-        checkPasswordMatch(password, password2);
+      checkLength(username, 3, 15);
+      checkLength(password, 6, 25);
+      checkEmail(email);
+      checkPasswordsMatch(password, password2);
     }
-});
+  
+  });
